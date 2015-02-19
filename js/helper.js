57052f22842967f1ea77e19e1375d81ad474a7be
thangthin <thangthin@gmail.com>
2015-02-19 13:29:20 -0500

    simplified map infowindow content

--- js/helper.js
+++ js/helper.js
@@ -65,7 +65,7 @@ The International Name challenge in Lesson 2 where you'll create a function that
 $(document).ready(function() {
   $('button').click(function() {
     var iName = inName() || function(){};
-    $('#name').html(iName);  
+    $('#name').html(iName);
   });
 });
 
@@ -168,13 +168,14 @@ function initializeMap() {
     // or hover over a pin on a map. They usually contain more information
     // about a location.
     var infoWindow = new google.maps.InfoWindow({
-      content: name + marker.position
+      content: name
+      //TODO: Add other interesting info to content
     });
 
     // hmmmm, I wonder what this is about...
     google.maps.event.addListener(marker, 'click', function() {
       // your code goes here!
-      
+
       infoWindow.open(map,marker);
     });
 
